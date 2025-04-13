72a5d040f2ef346bd8377835f56398c6954874cc
wingtatonfire <leewingtat78@gmail.com>
2025-04-13 12:54:05 -0700

    adding the checkWin method

--- gameboard.js
+++ gameboard.js
@@ -6,10 +6,27 @@ const gameboard = (function createGameboard() {
     }
     const putMarkX = function(index){
         gameboard[index] = "X";
+        this.checkWin("X");
     }
     const putMarkO = function(index){
         gameboard[index] = "O";
+        this.checkWin("O");
     }
-    return {gameboard, putMarkX, putMarkO}
+    const winCon = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+    const checkWin = function(player){
+        winCon.forEach((item)=>{
+            if (gameboard[item[0]] === player && gameboard[item[1]] === player && gameboard[item[2]] === player) {
+                console.log(player + " is the winner");
+            }
+        })
+    }
+
+    return {gameboard, putMarkX, putMarkO, checkWin}
 })()
+gameboard.putMarkX(0)
+gameboard.putMarkX(1)
+gameboard.putMarkX(2)
+
+
 
